9d9f9469967222c43318626cff136c5d845c42ff
AndrewGithinji <andrewgithinji@hotmail.com>
2023-06-28 12:25:30 +0300

    Helpers fix

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,23 +1,26 @@
-const redisClient = require('./redis');
-const dbClient = require('./db');
+import redisClient from './redis';
+import dbClient from './db';
 
 async function getAuthToken(request) {
   const token = request.headers['x-token'];
   return `auth_${token}`;
 }
 
+// checks authentication against verified information
+// returns userId of user
 async function findUserIdByToken(request) {
   const key = await getAuthToken(request);
   const userId = await redisClient.get(key);
   return userId || null;
 }
 
+// Gets user by userId
+// Returns exactly the first user found
 async function findUserById(userId) {
-  const userExistsArray = await dbClient.users.find({ _id: ObjectId(userId) }).toArray();
+  const userExistsArray = await dbClient.users.find(`ObjectId("${userId}")`).toArray();
   return userExistsArray[0] || null;
 }
 
-module.exports = {
-  findUserIdByToken,
-  findUserById,
+export {
+  findUserIdByToken, findUserById,
 };
